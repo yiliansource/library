eab1bd79e87e5d85e7f2bf0a139522044979d0ea
Ian Hornik <yiliansource@gmail.com>
2026-05-05 21:12:15 +0200

    improve layout, add metadata

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE files (
     file_id TEXT PRIMARY KEY,
     display_name TEXT,
     description TEXT,
+    tags TEXT,
     file_name TEXT NOT NULL UNIQUE,
     updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
     password_hash TEXT
